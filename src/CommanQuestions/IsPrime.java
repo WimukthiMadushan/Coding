eb3f7140c4c38f2a8b9fc08f2815ef1e5af33430
WimukthiMadushan <bandarawgwm.21@uom.lk>
2026-01-04 23:15:57 +0530

    is prime

--- src/CommanQuestions/IsPrime.java
+++ src/CommanQuestions/IsPrime.java
@@ -1,4 +1,15 @@
 package CommanQuestions;
 
 public class IsPrime {
+    public boolean isPrime(int num) {
+        if(num==1)
+            return false;
+        if(num==2)
+            return true;
+        for(int i = 2; i <= num - 1; i++){
+            if(num%i==0)
+                return false;
+        }
+        return true;
+    }
 }
